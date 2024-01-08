fa157331bff6d02cca10a93f2a2bfb806e7746e0
Parth faldu <pfaldu5@gmail.com>
2024-01-08 18:45:01 +0530

    Basics of Datatypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,27 @@
+"use strict";  // treat all JS Code as newer Version
+
+//  alert(3+3) // we are using nodejs , not browser
+
+console.log( 3 
+    +
+    3) //code readability should be high
+
+let name = "parth" // String
+let age = 25 // Number
+let isLoggedIn = true //boolean 
+
+
+// Primitive DataTypes
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true / false
+// null => standalong value / represent of empty value
+// undefined => unassinged value
+// symbol => unique 
+
+
+// Object Datatypes
+
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
