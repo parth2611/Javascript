41fffcc18fe222badc1d69af9237e742230b4b03
Parth faldu <pfaldu5@gmail.com>
2024-01-08 18:11:15 +0530

    Javascript variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,24 @@
+const accountID = 12334455
+
+let accountEmail = "parth@gmail.com"
+
+var accoundPassword = "12345"
+
+accountCity = "Ahmedabad"
+let accountState;
+
+// accountID = 2 // not allowed
+
+/*
+Prefer not to use var
+because of issue in block scope and function scope
+*/
+
+accountEmail = "pfaldu5@mail.com"
+accoundPassword = "123456"
+accountCity = "surat"
+
+console.log(accountID)
+
+
+console.table([accountID,accountEmail,accoundPassword,accountCity,accountState]);
